869682a3c85a7b30abb4490b91ba79c0a68f979d
OA Wu <comdan66@gmail.com>
2019-07-23 13:17:12 +0800

    調整 HTML Lib

--- System/Lib/Html.php
+++ System/Lib/Html.php
@@ -6,34 +6,31 @@ abstract class DomElement {
   private static $SingletonTags = ['area', 'base', 'br', 'col', 'command', 'embed', 'hr', 'img', 'input', 'keygen', 'link', 'meta', 'param', 'source', 'track', 'wbr'];
 
   private $tag;
-  protected $text, $attrs = [];
-  
-  public function __construct($text = '', $attrs = []) {
+  protected $text, $attrs = [], $datas = [];
+
+  public function __construct($text = '') {
     $this->tag = strtolower(deNamespace(get_called_class()));
     $this->text($text);
-    $this->attrs($attrs);
   }
-  
+
   public function text($text) {
     $this->text = $text;
     return $this;
   }
-  public function attrs($attrs, $excludes = []) {
-    if (is_string($attrs)) {
-      $this->attrs[$attrs] = $excludes;
-      return $this;
-    }
-    $tmps = [];
-    foreach ($attrs as $key => $attr)
-      if (!($excludes && in_array($key, $excludes)))
-        $tmps[$key] = $attr;
-    $this->attrs = $tmps;
+
+  public function data($key, $val) {
+    if ($val === null) return $this;
+    is_array($val) && $val = json_encode($val);
+    $this->datas[$key] = $val;
     return $this;
   }
-  
+
   public function __toString() {
     if ($this->tag === null)
       return '';
+
+    foreach ($this->datas as $key => $value)
+      $this->attrs['data-' . $key] = $value;
 
     if (in_array($this->tag, DomElement::$SingletonTags))
       return '<' . $this->tag . attr($this->attrs) . '/>';
@@ -41,18 +38,25 @@ abstract class DomElement {
     return '<' . $this->tag . attr($this->attrs) . '>' . (is_array($this->text) ? implode('', $this->text) : $this->text) . '</' . $this->tag . '>';
   }
 
-  public static function create($text = '', $attrs = []) {
-    return new static($text, $attrs);
+  public static function create($text = '') {
+    return new static($text);
   }
+
   public function __call($name, $arguments) {
     $this->attrs[$name] = array_shift($arguments);
     return $this;
   }
 }
 
-class A extends DomElement { public function target($target) { $this->attrs['target'] = $target; return $this; } }
+class A extends DomElement {}
 class B extends DomElement {}
+class I extends DomElement {}
+class H1 extends DomElement {}
 class Div extends DomElement {}
 class Span extends DomElement {}
 class Figure extends DomElement {}
 class Pre extends DomElement {}
+class Del extends DomElement {}
+class Option extends DomElement {}
+class Select extends DomElement {}
+class Label extends DomElement {}
