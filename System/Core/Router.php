dbc7559bd88176afe856277f1be6d285bc0f3b94
OA Wu <oawu.tw@gmail.com>
2025-01-05 00:58:18 +0800

    no message

--- System/Core/Router.php
+++ System/Core/Router.php
@@ -14,41 +14,69 @@ namespace Router {
     }
 
     public function uri($uri = null) {
-      if ($uri === null)
+      if ($uri === null) {
         return $this->uri;
+      }
 
-      $this->uri = $uri ?? '';
+      if ($uri !== null) {
+        $this->uri = $uri;
+      } else {
+        $this->uri = '';
+      }
+
       return $this;
     }
 
     public function mid($mid = null) {
-      if ($mid === null)
+      if ($mid === null) {
         return $this->mid;
+      }
 
-      $this->mid = $mid ?? '';
+      if ($mid !== null) {
+        $this->mid = $mid;
+      } else {
+        $this->mid = '';
+      }
+
       return $this;
     }
 
     public function dir($dir = null) {
-      if ($dir === null)
+      if ($dir === null) {
         return $this->dir;
+      }
 
-      $this->dir = $dir ?? '';
+      if ($dir !== null) {
+        $this->dir = $dir;
+      } else {
+        $this->dir = '';
+      }
+
       return $this;
     }
 
     public function name($name = null) {
-      if ($name === null)
-        return $this->name ?? $this->uri;
+      if ($name === null) {
+        if ($this->name !== null) {
+          return $this->name;
+        }
+        return $this->uri;
+      }
 
-      $this->name = $name ?? $this->uri;
+      if ($name !== null) {
+        $this->name = $name;
+      } else {
+        $this->name = $this->uri;
+      }
+
       return $this;
     }
-    
+
     public function items($closure) {
-      if (!is_callable($closure))
+      if (!is_callable($closure)) {
         return $this;
-      
+      }
+
       $closure();
       return $this;
     }
@@ -85,13 +113,19 @@ namespace {
       $this->groupNames = array_reduce(array_filter(array_map(function($group) { return $group->name(); }, $groups), function($name) { return $name !== '' && $name !== []; }), function($a, $b) { return array_merge($a, is_array($b) ? $b : [$b]); }, []);
 
       $this->segment = self::setSegment(($this->uris ? implode('/', $this->uris) . '/' : '') . trim($segment, '/'));
-      self::$routers[$method] = self::$routers[$method] ?? [];
+
+      if (!isset(self::$routers[$method])) {
+        self::$routers[$method] = [];
+      }
+
       return self::$routers[$method][$this->segment] = &$this;
     }
 
     public function title($title = null) {
-      if ($title === null)
+      if ($title === null) {
         return $this->title;
+      }
+
       $this->title = $title;
       return $this;
     }
@@ -103,24 +137,32 @@ namespace {
 
     public function controller($controller) {
       $controller = trim($controller, '/');
-      strpos($controller, '@') !== false || $controller = $controller . '@' . 'index';
+      if (strpos($controller, '@') === false) {
+        $controller = $controller . '@' . 'index';
+      }
+
       list($path, $this->method) = explode('@', $controller);
 
       $this->class = pathinfo($path, PATHINFO_BASENAME);
       $path = pathinfo($path, PATHINFO_DIRNAME);
-      $path === '.' || array_push($this->dirs, $path);
+
+      if ($path !== '.') {
+        array_push($this->dirs, $path);
+      }
 
       return $this->name(implode('', array_map('ucfirst', [$this->class, $this->method])));
     }
 
     public function name($name = null) {
-      if ($name === null)
+      if ($name === null) {
         return $this->name;
+      }
 
       $key = implode('', array_map('ucfirst', array_merge($this->groupNames, [$this->class, $this->method])));
 
-      if (isset(self::$names[$key]))
+      if (isset(self::$names[$key])) {
         unset(self::$names[$key]);
+      }
 
       return self::$names[$this->name = implode('', array_map('ucfirst', $this->groupNames)) . $name] = &$this;
     }
@@ -135,47 +177,70 @@ namespace {
     }
 
     public function __call($name, $arguments) {
-      if ($name == 'class')
+      if ($name == 'class') {
         return $this->class;
+      }
 
-      if ($name == 'method')
+      if ($name == 'method') {
         return $this->method;
+      }
 
-      if ($name == 'func')
+      if ($name == 'func') {
         return $this->func;
+      }
 
-      if ($name == 'mids')
+      if ($name == 'mids') {
         return $this->mids;
-      
-      if ($name == 'path')
+      }
+
+      if ($name == 'path') {
         return implode(DIRECTORY_SEPARATOR, array_merge($this->dirs, [$this->class]));
-      
+      }
+
       QQ('Router 不存在「' . $name . '」方法！');
     }
-    
+
     public static function group($options, $items = null) {
       return new \Router\Group($options, $items);
     }
 
     public static function init() {
-      return array_map(function($file) {
-        return !in_array($file, ['.', '..'])
-          ? !is_dir($path = PATH_ROUTER . $file)
-            ? pathinfo($file, PATHINFO_EXTENSION) == 'php'
-              ? Load::file(PATH_ROUTER . $file)
-              : null
-            : array_map(function($subfile) use ($path) {
-              return pathinfo($subfile, PATHINFO_EXTENSION) == 'php'
-                ? Load::file($path . DIRECTORY_SEPARATOR . $subfile)
-                : null;
-            }, @scandir($path) ?: [])
-          : null;
-      }, @scandir(PATH_ROUTER) ?: []);
+      $file1s = @scandir(PATH_ROUTER);
+
+      if ($file1s === false) {
+        $file1s = [];
+      }
+
+      foreach ($file1s as $file1) {
+        if (!in_array($file1, ['.', '..'])) {
+          $path = PATH_ROUTER . $file1;
+
+          if (pathinfo($file1, PATHINFO_EXTENSION) == 'php') {
+            Load::file(PATH_ROUTER . $file1);
+          }
+
+          if (is_dir($path)) {
+            $file2s = @scandir($path);
+
+            if ($file2s === false) {
+              $file2s = [];
+            }
+
+            foreach ($file2s as $$file2) {
+              if (!in_array($file2, ['.', '..'])) {
+                if (pathinfo($file2, PATHINFO_EXTENSION) == 'php') {
+                  Load::file($path . DIRECTORY_SEPARATOR . $file2);
+                }
+              }
+            }
+          }
+        }
+      }
     }
 
     private static function setSegment($segment) {
       static $keys, $vals;
-      
+
       if ($keys === null || $vals === null) {
         $keys = array_map(function($t) { return ':' . $t; }, array_keys(self::$regxPattern));
         $vals = array_map(function($t) { return ':' . $t; }, array_values(self::$regxPattern));
@@ -183,22 +248,39 @@ namespace {
 
       $segment = trim($segment, '/');
       $segment = str_replace($keys, $vals, $segment);
-      
+
       return preg_replace('/\((\w+?):/', '(?<$1>', $segment);
     }
 
     public static function __callStatic($name, $args) {
-      $methods = ['cli', 'get', 'post', 'put', 'delete', 'del', 'copy', 'head', 'options', 'link', 'unlink', 'purge'];
+      $methods = [
+        'cli',
+        'get',
+        'post',
+        'put',
+        'delete',
+        'del',
+        'head',
+        'options',
+        'patch',
+      ];
 
-      in_array($name = strtolower($name), $methods) || QQ('Router 不存在「' . $name . '」的 Static 方法！');
+      if (!in_array($name = strtolower($name), $methods)) {
+        QQ('Router 不存在「' . $name . '」的 Static 方法！');
+      }
 
       $name == 'del' && $name = 'delete';
-      $args || $args = [''];
+
+      if (!$args) {
+        $args = [''];
+      }
 
       $groups = array_reverse(array_map(function($trace) {
         return $trace['object'];
       }, array_filter(debug_backtrace(DEBUG_BACKTRACE_PROVIDE_OBJECT), function($trace) {
-        return isset($trace['function'], $trace['object']) && $trace['function'] == '__construct' && $trace['object'] instanceof \Router\Group;
+        return isset($trace['function'], $trace['object'])
+          && $trace['function'] == '__construct'
+          && $trace['object'] instanceof \Router\Group;
       })));
 
       return new Router($name, $groups, array_shift($args));
